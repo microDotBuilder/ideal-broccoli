2eea18602ca436a44a5023c792bf02bb11cde576
boringtutor <micro.builder.io@gmail.com>
2024-12-09 15:39:15 -0800

    working on socket io implementation

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,31 +1,51 @@
 import dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
+import { createServer } from "http";
+import { newErrorHandler } from "./middlewares/error-middleware";
+import { Server } from "socket.io";
 
 dotenv.config({
   path: "./.env",
 });
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+// TEST START
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
 
-// global middlewares
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  }),
-);
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+});
 
-import healthcheckRouter from "./routes/health-check-route";
-import userRouter from "./routes/user-route";
-import { newErrorHandler } from "./middlewares/error-middleware";
+//TEST END
 
-app.use("/api/v1/healthcheck", healthcheckRouter);
-app.use("/api/v1/user", userRouter);
+// // global middlewares
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
+// // TODO: Fix the error handler type
+// app.use(newErrorHandler);
 
-// TODO: Fix the error handler type
-app.use(newErrorHandler);
+// // global routes
+// app.use(express.json({ limit: "16kb" }));
+// app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-export default app;
+// // routes imports
+// import healthcheckRouter from "./routes/health-check-route";
+// import userRouter from "./routes/user-route";
+
+// // routes
+// app.use("/api/v1/healthcheck", healthcheckRouter);
+// app.use("/api/v1/user", userRouter);
+
+export default server;
--- back/src/index.ts
+++ back/src/index.ts
@@ -1,5 +1,5 @@
-import app from "./app";
+import server from "./app";
 
-app.listen(process.env.PORT || 8080, () =>
-  console.log("Server is running on port: " + process.env.PORT),
+server.listen(process.env.PORT || 8080, () =>
+  console.log("Server is running on port: " + process.env.PORT)
 );
